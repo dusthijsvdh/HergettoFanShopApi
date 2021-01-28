d798877d44c4cc2840ee44ad751aff7b2ef314a9
Thijs van der Heijden <thijsvdh12@gmail.com>
2021-01-28 04:06:24 +0100

    :sparkles: I'm done

--- routes/products.js
+++ routes/products.js
@@ -58,5 +58,38 @@ router.get("/:id", (req, res, next) => {
   });
 });
 
+router.put("/:id", checkAuth, (req, res, next) => {
+  if (req.user.admin) {
+    const product = new Product({
+      productId: req.autosan.body.productId,
+      title: req.autosan.body.title,
+      price: req.autosan.body.price,
+      description: req.autosan.body.description,
+      imageUrl: req.autosan.body.imageUrl,
+      sizes: req.autosan.body.sizes,
+      colors: req.autosan.body.colors,
+    });
+    Product.updateOne({ _id: req.params.id }, {
+      productId: req.autosan.body.productId,
+      title: req.autosan.body.title,
+      price: req.autosan.body.price,
+      description: req.autosan.body.description,
+      imageUrl: req.autosan.body.imageUrl,
+      sizes: req.autosan.body.sizes,
+      colors: req.autosan.body.colors,
+    }, {upsert: true}).then(result => {
+      res.status(200).json({message: "Update successful!" });
+    });
+  } else {
+    res.status(403).json({message: "Product wasn't updated" });
+  }
+});
+
+router.delete("/:id", checkAuth, (req, res, next) => {
+  Product.deleteOne({_id: req.params.id}).then(result => {
+    res.status(200).json({message: "Product deleted!" });
+  });
+});
+
 
 module.exports = router;
